c7552efc01c3fbbcf6584fd05bbae1c3eea0f091
Swapnil <bhosalesb07@gmail.com>
2025-08-16 18:00:20 +0530

    Section: Personal Information
    Text fields for Res and Re.
    
    A DatePicker for BirthDate (Aug 16, 2025, 5:57 PM).
    
    Section: Actions
    
    “Send Newsletter” (likely a toggle).
    
    “Number of …” (maybe a stepper or text field).
    
    “This video…” (possibly another setting).
    
    “Terms of Service” (red text → NavigationLink).
    
    Navigation bar
    
    Title: Account.
    
    Right side: save button.
    
    A keyboard-dismiss button above the keyboard.

--- SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var firstName:String = ""
     @State private var lastName:String = ""
     @State private var birthdate = Date()
+    @State private var shouldSendNewsLetter:Bool = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
         NavigationView{
             Form{
@@ -20,8 +23,30 @@ struct ContentView: View {
                     TextField("Last Name", text: $lastName)
                     DatePicker("BirthDate", selection: $birthdate)
                 }
+                Section(header:Text("Actions")){
+                    Toggle("Send News Letter", isOn: $shouldSendNewsLetter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100 )
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms of service" , destination: URL(string: "https://www.youtube.com/watch?v=m0QQ-hWs8fc")!)
+                }
+            }
+            .accentColor(.red)
+            .navigationTitle("Account")
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarTrailing){
+                    Button {
+                        hideKeyboard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    Button("save",action: saveuser)
+                }
             }
         }
+    }
+    func saveuser(){
+        print("save user")
     }
 }
 
@@ -30,3 +55,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+#if canImport(UIKit)
+extension View{
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
